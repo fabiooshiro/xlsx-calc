9b135973983ad22c4d679fd0c8ba9ba529933965
Oshiro <fabio.oshiro@calindra.com.br>
2020-09-04 21:02:05 -0300

    bump version

---
+++ test/6-conditional-tests.js
@@ -0,0 +1,28 @@
+const XLSX_CALC = require("../src");
+const assert = require("assert");
+
+xdescribe("verifica condicionais", () => {
+  it("valida ifs", () => {
+    let workbook = {
+      Sheets: {
+        Sheet1: { A1: {} }
+      }
+    };
+    workbook.Sheets.Sheet1.A1.f = "IF(AND(1<2,1<3),123,3)";
+    XLSX_CALC(workbook);
+    assert.equal(workbook.Sheets.Sheet1.A1.v, 123);
+  });
+  it("valida ifs", () => {
+    let workbook = {
+      Sheets: {
+        Sheet1: { A1: {} }
+      }
+    };
+    XLSX_CALC.set_fx("IS_IOS", () => {
+      return false;
+    });
+    workbook.Sheets.Sheet1.A1.f = "IF(AND(1<2,IS_IOS()),123,3)";
+    XLSX_CALC(workbook);
+    assert.equal(workbook.Sheets.Sheet1.A1.v, 123);
+  });
+});
